4e0e3674b2d082a642a39585dde8a8c31a7c7c3c
IloveColdCoffee <narek.manoukian0@gmail.com>
2022-09-11 21:28:50 +0400

    dfgh

---
+++ src/LoginWithHook/Hook.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import "./Class.css"
+
+export class Hook extends Component {
+     state = {login: "", password: ""}
+}
+     setLogin = (event) =>{
+      this.setState({login:event.target.value})
+     }
+     setPassword = (event) =>{
+      this.setState({password:event.target.value})
+     }
+     onSubmit = (event) =>{
+      event.preventDefault()
+    
+      alert("Yep")
+     }
+
+  
+  return (
+    <form onSubmit={this.onSubmit}>
+     
+      <label htmlFor="login">Login</label>
+      <input
+        onChange={this.setLogin}
+        id="login"
+        name="login"
+        type="text"
+      />
+      <label htmlFor="password">Password</label>
+      <input onChange={this.setPassword}
+        id="login"
+        name="password"
+        type="password"
+      />
+      <button type="submit">Log in</button>
+    </form>
+  )
